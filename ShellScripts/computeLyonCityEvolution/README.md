36b9ccf0c1988e1640d1cb00daa4493bd6dc99a8
jailln <vincentjaillot@gmail.com>
2019-04-30 11:18:11 +0200

    Add Shell scripts for Change detection computation on the city of Lyon and 3DCityDB utils scripts.

---
+++ ShellScripts/computeLyonCityEvolution/README.md
@@ -0,0 +1,27 @@
+# Compute Lyon city evolution in time
+
+The entry point is the script `computeLyonCityEvolution.sh`. It allows for the
+computation of the evolution of the city of Lyon from 2009 to 2015.
+
+Expected output:
+  * graphML-JSON files describing the transactions between features
+  of the vintages. There is four graphML-JSON per borrough (two for 2009-2012:
+  buildings and two for 2012-2015: buildings)
+
+*Note: The different processes are described inside the scripts.*
+*Disclaimer: Remarkable buildings are not considered*
+
+## Prerequisites
+
+ * Install the
+[EBO's working branch of 3DUSE](https://github.com/EricBoix/3DUSE/tree/ChangeDetection)
+ in this directory by following this
+[3DUSE install guide](https://github.com/EricBoix/3DUSE/blob/ChangeDetection/Install.md)
+
+  * [Install 3DCityDB](https://github.com/MEPP-team/RICT/blob/master/Install/Install3DCityDB.md)
+
+## Usage:
+
+Run the `computeLyonCityEvolution.sh` script which waits for an output folder
+and for the path to 3DUSE Build folder.
+Example: `computeLyonCityEvolution.sh LyonTemporalEvolution ./3DUSE/Build`
---
+++ ShellScripts/computeLyonCityEvolution/computeLyonCityEvolution.sh
@@ -0,0 +1,51 @@
+# !/bin/sh
+
+# This script detects the changes between CityGML files representing the
+# buildings of the city of Lyon at two different vintages and outputs the
+# changes as graphML-JSON files.
+
+# Awaited parameters:
+# * output folder for the intermdiate data and for the final result.
+# * path to 3DUSE Build folder
+
+# This script only works when invocated where it stands...
+cd "$(dirname "$0")" || exit
+
+# Check that parameters are correctly provided
+if [ $# != 2 ]
+  then
+	  echo "Awaited parameters: output folder and path to 3DUSE build folder"
+    exit 1
+fi
+
+# Create output directory
+mkdir ${1}
+
+# Download the data of the city of Lyon in 2009, 2012 and 2015 from the
+# [Data Grand Lyon open portal](https://data.grandlyon.com/) and patch the
+# errors in the files (e.g. buildings with empty geometry and textures with
+# wrong coordinates).
+./downloadPatchLyonCityGML.sh 2009 ${1}/Lyon_2009
+./downloadPatchLyonCityGML.sh 2012 ${1}/Lyon_2012
+./downloadPatchLyonCityGML.sh 2015 ${1}/Lyon_2015
+
+# Split the buildings of 2009 and 2012 (2015 don't need splitting as the
+# original buildings are already "splitted")
+./splitBuildingsLyonCityGML.sh ${1}/Lyon_2009 2009 ${1}/Lyon_2009_Splitted ${2}
+./splitBuildingsLyonCityGML.sh ${1}/Lyon_2012 2012 ${1}/Lyon_2012_Splitted ${2}
+
+# Extract buildings of 2015 from original GrandLyon folders organization
+mkdir ${1}/Lyon_2015_Splitted
+cp ${1}/Lyon_2015/LYON_1ER_2015/LYON_1ER_BATI_2015.gml ${1}/Lyon_2015_Splitted
+cp ${1}/Lyon_2015/LYON_2EME_2015/LYON_2EME_BATI_2015.gml ${1}/Lyon_2015_Splitted
+cp ${1}/Lyon_2015/LYON_3EME_2015/LYON_3EME_BATI_2015.gml ${1}/Lyon_2015_Splitted
+cp ${1}/Lyon_2015/LYON_4EME_2015/LYON_4EME_BATI_2015.gml ${1}/Lyon_2015_Splitted
+cp ${1}/Lyon_2015/LYON_5EME_2015/LYON_5EME_BATI_2015.gml ${1}/Lyon_2015_Splitted
+cp ${1}/Lyon_2015/LYON_6EME_2015/LYON_6EME_BATI_2015.gml ${1}/Lyon_2015_Splitted
+cp ${1}/Lyon_2015/LYON_7EME_2015/LYON_7_BATI_2015.gml ${1}/Lyon_2015_Splitted/LYON_7EME_BATI_2015.gml
+cp ${1}/Lyon_2015/LYON_8EME_2015/LYON_8EME_BATI_2015.gml ${1}/Lyon_2015_Splitted
+cp ${1}/Lyon_2015/LYON_9EME_2015/LYON_9EME_BATI_2015.gml ${1}/Lyon_2015_Splitted
+
+# extractBuildingDates: detect changes between two vintages of the city
+./extractBuildingDates.sh 2009 ${1}/Lyon_2009_Splitted 2012 ${1}/Lyon_2012_Splitted ${1}/final_output ${2}
+./extractBuildingDates.sh 2012 ${1}/Lyon_2012_Splitted 2015 ${1}/Lyon_2015_Splitted ${1}/final_output ${2}
