59a75e291619820eedc458cb131c919c690d2606
EricBoix <eric.boix@insa-lyon.fr>
2019-09-25 14:52:45 +0200

    Archive extracted data only considers .gml files

--- ShellScripts/computeLyonCityEvolution/Cwl/Download/DockerContext/collect_data.sh
+++ ShellScripts/computeLyonCityEvolution/Cwl/Download/DockerContext/collect_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# This script downloads the designated CityGML data file from Lyon
-# Metropole open data repository and when necessary applies some
-# ad-hoc fixes on well known inconsitancies.
+# This script downloads the designated CityGML archive files from Lyon
+# Metropole open data repository, extracts the files of the form 
+# *BATI*.gml and when necessary applies some  ad-hoc fixes on well
+# known inconsistancies.
 # $1: the filename to retrieved
 # $2: the resulting directory where to place the retrieved files
 
@@ -17,7 +18,7 @@ echo "Fetching file: ${filename}"
 wget -q ${filename}
 echo "Fetching done."
 echo "Unziping archive:"
-unzip -q ${1}
+unzip -q ${1} *BATI*.gml
 echo "Unziping done."
 rm ${1}
 
@@ -51,22 +52,28 @@ then
   # Remove the full blocks describing the buildings
   # <bldg:Building gml:id="LYON_8_00166"> and
   # <bldg:Building gml:id="LYON_8_00177">
+  echo "Patching file LYON_8EME_2009.gml"
   sed -e '1202763,1202789d' -e '1202791,1202817d' -i LYON_8EME_2009/LYON_8EME_BATI_2009.gml
+  echo "Patching done."
 fi
 
 if [ ${1} == "LYON_7EME_2012.zip" ]
 then
   # remove the full block describing the building
   # <bldg:Building gml:id="LYON_7EME_00215">
+  echo "Patching file LYON_7EME_2012.gml"
   sed -e '2752390,2752416d' -i LYON_7EME_2012/LYON_7EME_BATI_2012.gml
+  echo "Patching done."
 fi
 
 if [ ${1} == "LYON_8EME_2012.zip" ]
 then
-    # Replace occurences of -1.#IND00 in line 1099147
-    # (they do not represent a valide double) with a
-    # valid double e.g. -1.00
-    sed -e '1099147s/#IND00/00/g' -i LYON_8EME_2012/LYON_8EME_BATI_2012.gml
+  # Replace occurences of -1.#IND00 in line 1099147
+  # (they do not represent a valide double) with a
+  # valid double e.g. -1.00
+  echo "Patching file LYON_8EME_2012.gml"
+  sed -e '1099147s/#IND00/00/g' -i LYON_8EME_2012/LYON_8EME_BATI_2012.gml
+  echo "Patching done."
 fi
 
 echo "Collect_data.sh done."
